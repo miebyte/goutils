3de4c2a407757b4488895e02a677b708a91378e1
hoven <hovenyang.best@gmail.com>
2025-07-19 11:38:47 +0800

    update: grpc dialer

--- grpcutils/dialer.go
+++ grpcutils/dialer.go
@@ -10,7 +10,7 @@ package grpcutils
 
 import (
 	"context"
-	"time"
+	"net"
 
 	"github.com/miebyte/goutils/discover"
 	"github.com/miebyte/goutils/logging"
@@ -19,63 +19,57 @@ import (
 )
 
 func DialGrpc(service string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
-	return DialGrpcWithTimeOut(10*time.Second, service, opts...)
+	conn, err := dialGrpcWithTag(service, "", opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.Connect()
+	return conn, nil
 }
 
 func DialGrpcWithUnBlock(service string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	return dialGrpcWithTagContext(ctx, service, "", opts...)
+	return dialGrpcWithTag(service, "", opts...)
 }
 
 func DialGrpcWithTag(service string, tag string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
-	return DialGrpcWithTagTimeout(10*time.Second, service, tag, opts...)
+	return dialGrpcWithTag(service, tag, opts...)
 }
 
-func DialGrpcWithTimeOut(timeout time.Duration, service string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-	return DialGrpcWithContext(ctx, service, opts...)
-}
-
-func DialGrpcWithTagTimeout(timeout time.Duration, service, tag string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-	return DialGrpcWithTagContext(ctx, service, tag, opts...)
-}
-
-func DialGrpcWithContext(ctx context.Context, service string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
-	return dialGrpcWithTagContext(ctx, service, "", opts...)
-}
-
-func DialGrpcWithTagContext(ctx context.Context, service string, tag string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
-	return dialGrpcWithTagContext(ctx, service, tag, opts...)
-}
-
-func dialGrpcWithTagContext(ctx context.Context, service, tag string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
-	options := append(opts, defaultGRPCDialOptions()...)
-
-	address := discover.GetServiceFinder().GetAddressWithTag(service, tag)
+func dialGrpcWithTag(service, tag string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	options := append(opts, defaultGRPCDialOptions(tag)...)
+	options = append(options)
 
 	conn, err := grpc.NewClient(
-		address,
+		service,
 		options...,
 	)
 
-	if tag != "" {
-		logging.Debugc(ctx, "dial grpc service %s with tag %s. Addr=%s", service, tag, address)
-	} else {
-		logging.Debugc(ctx, "dial grpc service %s. Addr=%s", service, address)
-	}
+	conn.Connect()
+
 	return conn, err
 }
 
-func defaultGRPCDialOptions() []grpc.DialOption {
+func defaultGRPCDialOptions(tag string) []grpc.DialOption {
 	return []grpc.DialOption{
+		grpc.WithContextDialer(grpcConnDialFn(tag)),
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(1024 * 1024 * 16)),
 		grpc.WithChainUnaryInterceptor(unaryClientLoggerInterceptor()),
 		grpc.WithChainStreamInterceptor(streamClientLoggerInterceptor()),
+	}
+}
+
+func grpcConnDialFn(tag string) func(context.Context, string) (net.Conn, error) {
+	return func(ctx context.Context, service string) (net.Conn, error) {
+		address := discover.GetServiceFinder().GetAddressWithTag(service, tag)
+
+		if tag != "" {
+			logging.Debugc(ctx, "dial grpc service %s with tag %s. Addr=%s", service, tag, address)
+		} else {
+			logging.Debugc(ctx, "dial grpc service %s. Addr=%s", service, address)
+		}
+
+		return net.Dial("tcp", address)
 	}
 }

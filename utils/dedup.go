78a48b1a0b39e149015b7549ae5f65d7a270a9e4
superwhys <hovenyang.best@gmail.com>
2025-12-19 17:33:42 +0800

    feat: add dedup method

---
+++ utils/dedup.go
@@ -0,0 +1,41 @@
+package utils
+
+func Dedup[T comparable](slice []T) []T {
+	if len(slice) <= 50 {
+		return dupSmall(slice)
+	}
+	return dupLarge(slice)
+}
+
+func dupSmall[T comparable](slice []T) []T {
+	idx := 0
+	for _, s := range slice {
+		var j int
+		for j := range idx {
+			if slice[j] == s {
+				break
+			}
+		}
+		if j >= idx {
+			slice[idx] = s
+			idx++
+		}
+	}
+	return slice[:idx]
+}
+
+// dupLarge is the hashmap version of DupStrings with O(n) algorithm.
+func dupLarge[T comparable](slice []T) []T {
+	m := map[T]struct{}{}
+	idx := 0
+	for i, s := range slice {
+		if _, hit := m[s]; hit {
+			continue
+		} else {
+			m[s] = struct{}{}
+			slice[idx] = slice[i]
+			idx++
+		}
+	}
+	return slice[:idx]
+}
